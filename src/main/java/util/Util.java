be16b5a2671444b6cf61655cd2fdc92056062d53
%username% <%email%>
2024-05-26 06:19:51 -0600

    added package util with a new call Util

--- src/main/java/util/Util.java
+++ src/main/java/util/Util.java
@@ -1,4 +1,28 @@
 package util;
 
 public class Util {
+    /**
+     * converts each words in the inphut string to title case.
+     * @param strIn the input string to tile case.
+     * @returnthe dinput string with esch word converted to title case
+     */
+    public static String toTitleCase(String strIn){
+        if (strIn == null || strIn.isEmpty()){
+            return strIn;
+        }
+
+        String[] words = strIn.split(" ");
+        StringBuilder titleCaseString = new StringBuilder();
+
+        for (String word : words){
+            if (!word.isEmpty()){
+                if (!titleCaseString.isEmpty()){
+                    titleCaseString.append(" ");
+                }
+            } titleCaseString.append(Character.toUpperCase(
+                    word.charAt(0))).append(word.substring(1).toUpperCase());
+        }
+        return strIn;
+    }
+      return titleCaseString.toString();
 }
